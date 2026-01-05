e58aa5b510213ec43085ff76b7559c0ed6e786b3
Dawniyal <abbaszadeh.daniyal@gmail.com>
2026-01-05 17:44:19 +0330

    feat: add server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Dawniyal/webhookpipe/internal/config"
 	"github.com/Dawniyal/webhookpipe/internal/database"
+	"github.com/Dawniyal/webhookpipe/internal/lib/job"
+	"github.com/Dawniyal/webhookpipe/internal/server"
+	"github.com/redis/go-redis/v9"
 	"github.com/rs/zerolog"
 )
 
@@ -13,6 +19,8 @@ type Server struct {
 	Config     *config.Config
 	Logger     *zerolog.Logger
 	DB         *database.Database
+	Redis      *redis.Client
+	Job        *job.JobService
 	httpServer *http.Server
 }
 
@@ -22,10 +30,26 @@ func New(cfg *config.Config, logger *zerolog.Logger) (*Server, error) {
 		return nil, fmt.Errorf("failed to initialize database: %w", err)
 	}
 
+	redis := redis.NewClient(&redis.Options{
+		Addr:     cfg.Redis.Address,
+		Password: cfg.Redis.Password,
+		DB:       0,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := redis.Ping(ctx).Err(); err != nil {
+		logger.Error().Err(err).Msg("Failed to connect to Redis")
+		return nil, err
+	}
+
 	server := &Server{
 		Config: cfg,
 		Logger: logger,
 		DB:     db,
+		Redis:  redis,
+		Job:    jobService,
 	}
 
 	return server, nil
@@ -39,4 +63,32 @@ func (s *Server) SetupHttpServer(handler http.Handler) {
 		WriteTimeout: s.Config.Server.WriteTimeout,
 		IdleTimeout:  s.Config.Server.IdleTimeout,
 	}
+}
+
+func (s *Server) Start() error {
+	if s.httpServer == nil {
+		return errors.New("HTTP server not initialized")
+	}
+
+	s.Logger.Info().
+		Str("port", s.Config.Server.Port).
+		Msg("starting server")
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("failed to shutdown HTTP server: %w", err)
+	}
+
+	if err := s.DB.Close(); err != nil {
+		return fmt.Errorf("failed to close database connection: %w", err)
+	}
+
+	if s.Job != nil {
+		s.Job.Stop()
+	}
+
+	return nil
 }
